6857c4b8ac303d61767bac27790e38b05e413ed4
msabil443 <76219889+msabil443@users.noreply.github.com>
2023-08-06 12:15:03 +0530

    Created schema and connected to MongoDB

--- routes/hotel.router.js
+++ routes/hotel.router.js
@@ -1,11 +1,16 @@
 const express = require ('express');
 const router = express.Router();
-const hotels = require("../data/hotels")
-router.route("/")//localhost:3500/data/hotels/hotelCategory
-    .get((req, res)=> {
-        res.json(hotels.data);
-    }
-    )
+const Hotel = require("../model/hotel.model")
+router.route("/")
+    .get(async (req, res)=> {
+        try{
+            const hotels = await Hotel.find({});
+            hotels?res.json(hotels):res.status(404).json({message:"No data found"});
+        }catch(err){
+            console.log(err);
+        }
+
+    })
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require ('express');
 
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+const cors = require("cors");
+dotenv.config();
+const hotelDataAddedToDBRouter = require("./routes/dataimport.router");
 const hotelRouter = require("./routes/hotel.router");
 
+
+const connectDB = require('./config/dbconfig');
 const app = express();
+// app.use("/api/hoteldata", hotelDataAddedToDBRouter);
+// const connectDB = require("./config/dbconfig");
 
+app.use(cors());
 app.use(express.json());
-
+connectDB();
 const PORT = 3500;
 
 app.get("/",(req, res)=>{
@@ -13,9 +23,14 @@ app.get("/",(req, res)=>{
     res.send("Hello world")
 })
 
-app.use("/api/hotels", hotelRouter);
 
+app.use("/api/hoteldata", hotelDataAddedToDBRouter);//this is usedc to add data to the database
+app.use("/api/hotels", hotelRouter);//the hotels data is used here
 
-app.listen (process.env.PORT || PORT, ()=>{
-    console.log("Server is UP and Running")
+mongoose.connection.once("open", ()=>{
+    console.log("connected to the database");
+    app.listen (process.env.PORT || PORT, ()=>{
+        console.log("Server is UP and Running")
+    })
 })
+
